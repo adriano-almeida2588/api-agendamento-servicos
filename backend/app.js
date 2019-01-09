519f98820bd59345ed917bff0af892436c900a03
Adriano Almeida <adriano.almeida2588@gmail.com>
2019-01-09 19:25:43 -0300

    Adição de rotas de clientes
    Signed-off-by: Adriano Almeida <adriano.almeida2588@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 import { Server } from "hapi"
-import {servicos} from './src/routes/servicos'
-import {usuarios} from './src/routes/usuarios'
+import { servicos } from './src/routes/servicos'
+import { usuarios } from './src/routes/usuarios'
+import { clientes } from "./src/routes/clientes";
 
 const server = new Server({
     host: 'localhost',
     port: 3000
 })
 
-server.route(servicos)
+server.route(clientes)
 
 server.start()
 console.log(`Servidor executando na porta: ${server.info.port}`)
